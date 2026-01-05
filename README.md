c81e33743113afd3465c3555f1570cce7b7fca87
blackbhc <blackbhc@icloud.com>
2026-01-05 15:30:37 +0100

    integrate highfive

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Read a standard GADGET4 snapshot file (in HDF5 format) and get the corresponding
 radial force at specified polar grid. It require the galactic center at the origin
 and the disk located in the X-Y plane.
 
-______________________________________________________________________
+---
 
 ## Dependencies
 
@@ -14,19 +14,26 @@ ______________________________________________________________________
 
 - [`HDF5`](https://www.hdfgroup.org/solutions/hdf5/) for file IO.
 
-- [`CMake`](https://cmake.org/) >= 3.12
+- [`CMake`](https://cmake.org/) >= 3.13
+
+- [`fmt`](https://github.com/fmtlib/fmt.git) for format output, included as a git
+  sub module.
+
+- [`highfive`](https://github.com/highfive-devs/highfive.git) for HDF5 file IO,
+  included as a git sub module.
+
+- (Already integrated) [`cmdline`](https://github.com/tanakh/cmdline) for command
+  line parser.
 
 - (optional) [`GoogleTest`](https://github.com/google/googletest) for unit test.
 
-- (Already integrated) [`cmdline`](https://github.com/tanakh/cmdline) for command line parser.
-
-______________________________________________________________________
+---
 
 ## Installation
 
 To be complemented.
 
-______________________________________________________________________
+---
 
 ## Usage
 
--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,12 @@
 
 #include "args_parser.hpp"
 #include "grid.hpp"
-#include "h5io.hpp"
 #include <cstdlib>
 #include <fmt/base.h>  // for format print
+#include <highfive/H5Easy.hpp>
 #include <string_view>
 using namespace std::string_view_literals;
+using h5file = H5Easy::File;
 
 int main(int argc, char* argv[])
 {
@@ -22,8 +23,30 @@ int main(int argc, char* argv[])
     PolarGrid targetLocs(paras);
 
     // read the hdf5 snapshot file
-    H5IO snapFile("snapshot_000.hdf5"sv, H5IO::filemode::read);
+    h5file snapshot("snapshot_000.hdf5", h5file::ReadOnly);
+    // get the particle number of each type
+    auto partNums = H5Easy::loadAttribute<std::vector<int>>(snapshot, "Header",
+                                                            "NumPart_ThisFile");
+    /* for (auto num : partNums)
+    {
+        fmt::println("{} ", num);
+    } */
+    // read the coordinates and mass of each type
+    std::vector<double> mass;
+    auto                n = partNums[1];
+    mass.resize(n);
+    using Matrix = std::vector<std::vector<double>>;
+    Matrix coordinate(n, std::vector<double>(3));
+    mass.resize(partNums[1]);
 
+    mass = H5Easy::load<std::vector<double>>(snapshot, "/PartType1/Masses");
+    coordinate = H5Easy::load<Matrix>(snapshot, "/PartType1/Coordinates");
+
+    /* for (int i = 100; i < 110; i++)
+    {
+        fmt::println("m={}, (x, y, z)=({}, {}, {})", mass[i], coordinate[i][0],
+                     coordinate[i][1], coordinate[i][2]);
+    } */
     // get the snapshot data
     // std::vector<> datasets{read_coordiantes_and_masses()};
 
@@ -31,10 +54,21 @@ int main(int argc, char* argv[])
     // auto resutls = calculate_radial_force(datasets, target_locs);
 
     // write the results to the log file
-    H5IO rcFile("output.hdf5"sv, H5IO::filemode::write);
+    // H5IO rcFile("output.hdf5"sv, H5IO::filemode::write);
     // h5io.write_dataset(results);
 
-    fmt::println("Hello world from {{calrc}}.");
+    h5file              file("output.hdf5", h5file::Truncate);
+    std::vector<double> mock_rs(10, 2.12);
+    std::vector<double> mock_phis(10, 3.14 / 2);
+    std::vector<double> mock_frs(10, 101);
+    H5Easy::dump(file, "rs", mock_rs);
+    H5Easy::dump(file, "phis", mock_phis);
+    H5Easy::dump(file, "frs", mock_frs);
+
+    H5Easy::dumpAttribute(file, "/", "Rmin", 0.0);
+    H5Easy::dumpAttribute(file, "/", "Rmax", 10.0);
+    H5Easy::dumpAttribute(file, "/", "Rbinnum", 20);
+    H5Easy::dumpAttribute(file, "/", "PhiBinnum", 16);
 
     return EXIT_SUCCESS;
 }
